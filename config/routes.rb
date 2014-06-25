fc3fe082d7f5413239fd319ba032c48005e3ce96
B Haun <bhauncap@gmail.com>
2014-06-25 15:09:19 +0200

    Added dashboard page, bootswatch theme, pg database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "dashboard" => "pages#dashboard"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
